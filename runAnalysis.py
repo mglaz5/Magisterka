f5b49c81c8c023348cd358447b7cd4ae7b72c80c
Marianna Glazewska <marianna.glazewska@cern.ch>
2022-12-30 15:38:40 +0100

    Updated version of run file

--- runAnalysis.py
+++ runAnalysis.py
@@ -10,7 +10,7 @@ process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(1)
 process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(False))
 
-dataDir='/scratch/CMSSW_12_3_0/src/Projekt/Data/'
+dataDir='/eos/user/m/mglazews/Data/'
 lsCommand='ls -1 '+dataDir+'|grep root'
 print ('command: ',lsCommand)
 dir=subprocess.Popen(lsCommand, stdout=subprocess.PIPE,shell=True,text=True)
@@ -28,13 +28,16 @@ print (len(files))
 # input files (up to 255 files accepted)
 process.source = cms.Source('PoolSource',
 fileNames = cms.untracked.vstring( 
-#'/store/mc/Phase2HLTTDRSummer20ReRECOMiniAOD/GluGluHToGG_M125_14TeV_powheg_pythia8_TuneCP5//GEN-SIM-DIGI-RAW-MINIAOD/NoPU_111X_mcRun4_realistic_T15_v1-v1/270000/073371ED-128E-E741-A5D4-6C3C87E14820.root',
-'file:FEVTSIM.root' 
+'file:FEVTSIM.root'
 ),
-skipEvents =  cms.untracked.uint32(0)
+skipEvents =  cms.untracked.uint32(0),
+inputCommands=cms.untracked.vstring( #Added to avoid problematic library for FEVTSIM_1.root file
+                             "keep *",
+                             "drop l1tTrackerMuons_L1TkMuonsGmt__fullsim"
+)
 )
 #process.source.fileNames = files
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1))
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1)) #remember to change to -1! If 1, I was testing something
 
 process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
 process.load('Configuration.Geometry.GeometryDB_cff')
@@ -55,7 +58,8 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
 
-process.praca= cms.EDAnalyzer("Projekt")
+process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
 
+process.praca= cms.EDAnalyzer("Projekt")
 process.MyPath = cms.Path(process.praca)
 process.schedule = cms.Schedule(process.MyPath)
